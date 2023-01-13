568e95016c5993fd36e2ec5a0f3f8f6478017963
poow810 <poow810@naver.com>
2023-01-13 16:33:35 +0900

    day2 example-2

---
+++ day2/day2_ex-2.py
@@ -0,0 +1,12 @@
+small = []
+green = []
+if small:
+    if green:
+        print("완두콩은 작고 녹색이다.")
+    else:
+        print("체리는 작고 녹색이 아니다.")
+else:
+    if green:
+        print("수박은 크고 녹색이다.")
+    else:
+        print("호박은 크고 녹색이 아니다.")
