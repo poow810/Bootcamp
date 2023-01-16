a3e5fef0401b253a71fa605a523ea80abdfe41b1
poow810 <poow810@naver.com>
2023-01-16 17:16:06 +0900

    day3_ex6-2

--- day3/day3_ex.py
+++ day3/day3_ex.py
@@ -1,7 +1,13 @@
-# 5.8
-Boat = 'Duck'
-Horse = 'Gourd'
-Train = 'Spitz'
-print(f"an English submarine 이름은 ({Boat}y Mc{Boat}face)로 지어졌다.")
-print(f"an English submarine 이름은 ({Horse}y Mc{Horse}face)로 지어졌다.")
-print(f"an English submarine 이름은 ({Train}y Mc{Train}face)로 지어졌다.")
+# 6.2
+guess_me = 7
+number = 1
+while True:
+    if guess_me > number:
+        print("'too low'")
+    elif guess_me == number:
+        print("'found it!'")
+        break
+    else:
+        print("'oops'")
+        break
+    number +=1
