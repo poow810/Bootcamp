3379afb8f1e2b4a4d38a7d430c97b2dcbfd6af05
poow810 <poow810@naver.com>
2023-01-16 17:18:22 +0900

    day3_ex6-3

--- day3/day3_ex.py
+++ day3/day3_ex.py
@@ -1,13 +1,11 @@
-# 6.2
-guess_me = 7
-number = 1
-while True:
-    if guess_me > number:
+# 6.3
+guess_me = 5
+for number in range(10):
+    if number < guess_me:
         print("'too low'")
-    elif guess_me == number:
+    elif number == guess_me:
         print("'found it!'")
         break
     else:
         print("'oops'")
         break
-    number +=1
