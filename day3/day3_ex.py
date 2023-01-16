493e4cd7d492376fb1129ee37f542f0a02359246
poow810 <poow810@naver.com>
2023-01-16 17:09:37 +0900

    day3_ex5-8

--- day3/day3_ex.py
+++ day3/day3_ex.py
@@ -1,7 +1,7 @@
-# 5.7
+# 5.8
 Boat = 'Duck'
 Horse = 'Gourd'
 Train = 'Spitz'
-print("an English submarine 이름은 ({}y Mc{}face)로 지어졌다." .format(Boat,Boat))
-print("an English submarine 이름은 ({}y Mc{}face)로 지어졌다." .format(Horse, Horse))
-print("an English submarine 이름은 ({}y Mc{}face)로 지어졌다." .format(Train, Train))
+print(f"an English submarine 이름은 ({Boat}y Mc{Boat}face)로 지어졌다.")
+print(f"an English submarine 이름은 ({Horse}y Mc{Horse}face)로 지어졌다.")
+print(f"an English submarine 이름은 ({Train}y Mc{Train}face)로 지어졌다.")
