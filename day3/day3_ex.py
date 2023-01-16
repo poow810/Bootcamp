966581d5cea599fc11f2a2b71bd018f4b5a59610
poow810 <poow810@naver.com>
2023-01-16 16:56:49 +0900

    day3_ex5-5

--- day3/day3_ex.py
+++ day3/day3_ex.py
@@ -1,7 +1,8 @@
 # 5.4
 letter = "\tDear {salutation} {name}, " \
          "\n\n\tThank you for your letter. We are sorry that our {product} {verbed} in your " \
-         "\n{room}. Please note that it should never be used in a {room}, especially near any {animals}. \n\n\tSend us your receipt and {amount} for shipping and handling.', 'We will send you \nanother {product} that, in our test, is {percent}% less likely to have {verbed}. " \
+         "\n{room}. Please note that it should never be used in a {room}, especially near any {animals}. " \
+         "\n\n\tSend us your receipt and {amount} for shipping and handling.', 'We will send you \nanother {product} that, in our test, is {percent}% less likely to have {verbed}. " \
          "\n\n\tThank you for your support. " \
          "\n\tSincerly \n\t{spokesman} \n\t{job_title}"
-print(letter)
+print(letter.format(salutation="hi", name="하운", product="present", verbed="put", room="bed", animals="dog", amount="count", percent=50, spokesman="people", job_title="manager"))
