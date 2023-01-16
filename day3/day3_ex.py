1f715747778537b7089e4f81a9594c0911b74244
poow810 <poow810@naver.com>
2023-01-16 16:22:12 +0900

    day3 ex5-4

---
+++ day3/day3_ex.py
@@ -0,0 +1,9 @@
+# 5.4
+letter = ['salutation', 'name', 'product', 'verbed', 'room', 'animals', 'amount', 'product', 'percent', 'verbed', 'spokesman', 'job_title']
+print('\tDear {salutation} {name}')
+print('')
+print(('\tThank you for your letter. We are sorry that our {product} {verbed} in your \n{}.'), ('Please note that it should never be used in a {room}, especially near any {animals}'))
+print('')
+print('\tSend us your receipt and {amount} for shipping and handling.', 'We will send you \nanother {product} that, in our test, is {percent}% less likely to have {verbed}.')
+print('')
+print('\tThank you for your support.', '\n\tSincerly,', '\n\t{spokesman}', '\n\t{job_title}')
