fab7f196695abf1b066c615532b1237305c5585b
poow810 <poow810@naver.com>
2023-01-16 10:21:21 +0900

    day03 string 1

---
+++ day3/string.py
@@ -0,0 +1,18 @@
+print("I'm a boy")  # 앞의 I까지만 문자열로 생각
+
+
+army = '우리는 국가와 국민에 충성을 다하는 대한민국 육군이다. ' \
+       '하나 우리는 자유민주주의를 수호하며 조국 통일의 역군이 된다.'
+
+print(army)
+
+# 안쪽에 인용구를 넣고 싶을 때
+testimony = "\"I did nothing!\" he said"
+print(testimony)
+
+start = 'Na' *4 + '\n'
+middle = '헤이' *3 +'\n'
+end = '안녕'
+print(start+middle+end)
+# name = 'Henny'
+
