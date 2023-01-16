1c4162f69e48939d14049e9dd480e92a553e00e2
poow810 <poow810@naver.com>
2023-01-16 17:06:30 +0900

    day3_ex5-6

--- day3/day3_ex.py
+++ day3/day3_ex.py
@@ -1,8 +1,7 @@
-# 5.4
-letter = "\tDear {salutation} {name}, " \
-         "\n\n\tThank you for your letter. We are sorry that our {product} {verbed} in your " \
-         "\n{room}. Please note that it should never be used in a {room}, especially near any {animals}. " \
-         "\n\n\tSend us your receipt and {amount} for shipping and handling.', 'We will send you \nanother {product} that, in our test, is {percent}% less likely to have {verbed}. " \
-         "\n\n\tThank you for your support. " \
-         "\n\tSincerly \n\t{spokesman} \n\t{job_title}"
-print(letter.format(salutation="hi", name="하운", product="present", verbed="put", room="bed", animals="dog", amount="count", percent=50, spokesman="people", job_title="manager"))
+# 5.6
+Boat = 'Duck'
+Horse = 'Gourd'
+Train = 'Spitz'
+print("an English submarine 이름은 (%sy Mc%sface)로 지어졌다." %(Boat,Boat))
+print("an English submarine 이름은 (%sy Mc%sface)로 지어졌다." %(Horse, Horse))
+print("an English submarine 이름은 (%sy Mc%sface)로 지어졌다." %(Train, Train))
