beb52dadb99e3c68c0d73a101539d156701b73b3
poow810 <poow810@naver.com>
2023-01-16 15:36:09 +0900

    while-3

--- day3/while.py
+++ day3/while.py
@@ -42,3 +42,17 @@ if is_prime:
 else:
     print(f'{number} is not prime number.')
 
+# input 2 numbers
+
+start = int(input("start number : "))
+end = int(input("end number :"))
+if end < start :
+    start, end = end, start
+for i in range(start, end+1):
+    if i <= 1 :
+        continue
+    for k in range(2, i):
+        if i % k == 0:
+            break
+    else:
+        print(i, end=' ')
