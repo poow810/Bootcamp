92d9160e434753c1d7e9598a810b7e98046ae385
poow810 <poow810@naver.com>
2023-01-17 18:23:44 +0900

    day4 8-6~8-10

--- day4/day4_ex.py
+++ day4/day4_ex.py
@@ -1,21 +1,12 @@
-# 8.1
-ef2 = {'dog' : 'chine', 'cat' : 'chat', 'walrus' : 'morse'}
-print(ef2)
+# 8.6
+life = {'animal':{'cats':'Henri', 'octopi':'Grumpy', 'emus':'Lucy'}, 'plants':{}, 'other':{}}
+# 8.7
+print(life.keys())
+# 8.8
+print(life['animal'].keys())
+# 8.9
+print(life['animal']['cats'])
+# 8.10
 
-# 8.2
-print(ef2['walrus'])
-
-# 8.3
-list(ef2.items())
-f2e = dict(ef2)
-print(f2e)
-
-# 8.4
-for k, v in ef2.items():
-    print(k)
-    break       #break로 멈추는 방법 말고 첫번째만 얻는 방법?
-print(list(ef2.keys())[0])
-
-# 8.5
-for k in ef2.keys():
-    print(k, end=" ")
+squares = {i:i**2 for i in range(10)}
+print(squares)
