f6f5aa6ad57f61b790a90c6f19f4ed20b15e3d57
poow810 <poow810@naver.com>
2023-01-17 17:24:11 +0900

    day4 7-11

--- day4/day4_ex.py
+++ day4/day4_ex.py
@@ -1,3 +1,16 @@
-# 7.10
-numbers_list = [number for number in range(10) if number % 2 ==0]
-print(numbers_list)
+# 7.11
+start1 = ["fee", "fie", "foe"]
+rhymes = [
+    ("flop", "get a mop"),
+    ("fope", "turn the rope"),
+    ("fa", "get your ma"),
+    ("fudge", "call the judge"),
+    ("fat", "pet the cat"),
+    ("fog", "walk the dog"),
+    ("fun", "say we're done"),
+]
+start2 = "Someone better"
+start1 = '! '.join(start1)
+start1 = start1.title()
+print(start1+'! '+ rhymes[0][0].capitalize()+'!')
+print(start2 +' ' +rhymes[0][1]+'.')
