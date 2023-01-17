475f9125504043aff186c569916cd9f0900ced06
poow810 <poow810@naver.com>
2023-01-17 13:05:43 +0900

    tuple_list1

--- day4/tuple_list.py
+++ day4/tuple_list.py
@@ -1,57 +1,30 @@
-# tuple
-a = 'harry',
-b = ('harry',)
-c = ()      # empty tuple
-d = 'harry', 'ron'  # packing
-e = ('hermione')  # string
-f = ('harry', 'ron')  # packing
-g = ('hermione',)  # string
-print(id(g))    #id값이 다름
-print(f[1])
-x, y = f    # unpacking
-g = f+g     # 덮어쓰기
-print(id(g))    #id값이 다름
-print(g)
-
-
-# tuple 비교하기 - 원소 개수 비교
-a = (7, 2)
-b = (7, 2, 9)
-print(a == b)
-
 # list
-scores = ('B+', 'A+', 'C+')
-print(scores[1], scores[2])
-temp = list(scores)
-temp[1]='C+'
-temp[2]='A+'
-scores = tuple(temp)
-print(scores[1], scores[2])     # tuple값을 list로 변환, 수정 후 다시 tuple로
+# shallow copy
+a = [1, 2, [5, 6]]
+b = a.copy()
+c = list(a)
+d = a[:]
+a[1] = -77          # immutable한 원소가 변경되면 복사본은 변경 x
+a[2][1] = '7'       # 리스트 자체의 원소가 변경되면 복사본들도 변경 = 얕은 복사
+print(a, b, c, d)       # 나머지 복사본들은 바뀌지 않음
 
-# list
-big_bang = ['GD', '태양', '탑', '대성', '승리']
-# big_bang.append('인하')         # append는 맨 뒤에 넣음
+# deep copy
+import copy
+a = [1, 2, [5, 6]]
+b = copy.deepcopy(a)
+a[2][1] = 7
+print(a, b)         # 복사본은 바뀌지 않음
 
-big_bang.insert(1, '인하')        # insert는 특정 위치에 넣음
-# print(big_bang)
-exo = ['백현', '첸']
-# exo.extend(big_bang)       # 서로 다른 리스트를 병합
-# print(exo)
-exo.append(big_bang)         # 리스트안에 리스트가 들어오도록 추가
-print(exo)
-print(exo[2][2])             # 첫 리스트 1번에 있는 리스트 2번을 호출
-print(exo[-1][-4])
-exo[-2] = '시우민'
-# 삭제하기
-# print(exo.pop())         # 빅뱅 다 날라감 #pop은 맨 뒤 삭제 후 return
-print(exo[2].pop())
-print(exo)
-print(exo[2].pop(-2))
-print(exo)
-del exo[2][-1]      # 대성 delate
-print(exo)
+primes = [2, 19, 3, 5, 7, 11]
+primes_cp = primes
+print(primes)
+print(primes_cp)
+primes[-1] = 'lunch time'
+print(primes)
+print(primes_cp)
 
-exo[2].remove('인하')
-print(exo)
-exo.clear()
-print(exo)
+# mixed = [6, 4, 5, 'A+', 7, '트와이스', '8', 'b', '마마무'] # type error
+# mixed = ['6', '4', '5', 'A+', '7', '트와이스', '8', 'b', '마마무']
+# mixed.sort()
+# mixed.sort(reverse=True)
+# print(mixed)
---
+++ example/ex.py
@@ -0,0 +1,7 @@
+s=list(input().split())
+add = 0
+list_a = []
+for i in s:
+    list_a.append(len(i))
+answer = list_a
+print(answer)
