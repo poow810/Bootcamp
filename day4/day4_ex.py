3fc96f7eeabff5e64b3c07195e90ae07716e6c54
poow810 <poow810@naver.com>
2023-01-17 13:54:36 +0900

    day4 7-5, 7-6

--- day4/day4_ex.py
+++ day4/day4_ex.py
@@ -1,3 +1,9 @@
-# 7.4
+# 7.5 ~ 7.6
 things = ["mozzarella", "cinderella", "salmonella"]
+things[-2] = things[-2].title()     # cinderella 첫 글자만 대문자로 변환
 print(things)
+things[0] = things[0].upper()       # mozzarella를 대문자로 변환
+print(things)
+things.pop(2)       # salmonella 제거
+print(things)
+
