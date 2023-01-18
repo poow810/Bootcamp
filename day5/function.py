c9942b60afda0937624563038c93895b5446a05b
poow810 <poow810@naver.com>
2023-01-18 13:05:17 +0900

    function2

--- day5/function.py
+++ day5/function.py
@@ -1,24 +1,27 @@
 # function
-
-def isprime(n):
+def calculate_fee(*args):
     """
-    n이 소수인지의 여부 판단
-    :param n: integer number
-    :return: True or False
+    놀이공원 요금 계산 프로그램
+    :param args: 나이
+    :return: 총 금액
     """
-    if n <= 1:
-        return False
-    for k in range(2, n):
-        if n % k == 0:
-            return False
-    else:
-        return True
+    total = 0
+    for age in args:
+        if 19 <= age:
+            total = total + 10000
+        else:
+            total = total + 3000
+    return total
+print(calculate_fee(20, 20, 25))
+print(calculate_fee(45, 43, 10, 7))
 
-help(isprime)
-start = int(input("start number : "))
-end = int(input("end number : "))
-if end < start:
-    start, end = end, start
-for i in range(start, end + 1):
-    if isprime(i):
-        print(i, end=', ')
+
+# def do_nothing():
+#     pass
+#
+# mamamoo = {'화사', '솔라', '휘인', '문별'}
+# print(mamamoo.pop())        # 삭제할 값 리턴 후 삭제
+# print(mamamoo.remove('문별'))     #
+#
+#
+# print(mamamoo)
