e4193aa0279709a1e07de52f7fb4f3b53bdd3e2b
poow810 <poow810@naver.com>
2023-01-18 15:28:03 +0900

    function05

--- day5/function.py
+++ day5/function.py
@@ -1,33 +1,16 @@
-# function
+# Closures
+def calculate():
+    x = 1
+    y = 2
+    temp = 0
+    def add_sub(n):
+        nonlocal temp       # temp가 변경된 값을 계속 기억 != local variable
+        # x = 11      # local variable
+        temp = temp + x + n - y
+        return temp
+    return add_sub
 
-def inha():
-    """
-    숫자 출력
-    :return:
-    """
-    print(42)
-
-def call_func(f):
-    """
-    매개변수로 함수를 넘겨받아 실행
-    :param f: 매개변수가 함수
-    :return:
-    """
-    f()     # 넘겨 받은 함수 실행
-
-def subtract(n1, n2):
-    print(n1-n2)
-
-def run_func(f, arg1, arg2):
-    """
-    함수를 매개 변수로 받아 함수 안에서 해당 함수를 실행
-    :param f: 첫 번째 인수는 함수
-    :param arg1: 정수 값
-    :param arg2: 정수 값
-    :return:
-    """
-    f(arg1, arg2)
-run_func(subtract, 99, 88)
-
-call_func(inha)
-print(type(call_func))
+c1 = calculate()
+for i in range(5):
+    print(c1(i))
+print(type(c1))
