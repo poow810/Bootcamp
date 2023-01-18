ca0a09fa5ffb34f87f571659caebedeaae0d71ae
poow810 <poow810@naver.com>
2023-01-18 15:43:53 +0900

    function06

--- day5/function.py
+++ day5/function.py
@@ -1,16 +1,12 @@
-# Closures
-def calculate():
-    x = 1
-    y = 2
-    temp = 0
-    def add_sub(n):
-        nonlocal temp       # temp가 변경된 값을 계속 기억 != local variable
-        # x = 11      # local variable
-        temp = temp + x + n - y
-        return temp
-    return add_sub
+# lambda
 
-c1 = calculate()
-for i in range(5):
-    print(c1(i))
-print(type(c1))
+import random
+
+def process(no_list, f):
+    for no in no_list:
+        print(f(no))
+
+
+numbers = [random.randint(1,100) for i in range(5)]
+print(numbers)
+process(numbers, lambda x: x*x)
