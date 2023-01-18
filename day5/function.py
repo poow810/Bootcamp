1361da85919b2f18f7cbd0b0a96451e58d81617a
poow810 <poow810@naver.com>
2023-01-18 15:06:54 +0900

    function4

--- day5/function.py
+++ day5/function.py
@@ -1,62 +1,33 @@
 # function
-import random
 
-# 리스트로 표현
-def calculate_fee(args)->list:
+def inha():
     """
-    놀이공원 요금 계산 프로그램
-    :param args: age in list
-    :return: 전체 인원수, 어른 수, 아이수, 총 금액
+    숫자 출력
+    :return:
     """
-    total = 0
-    adult = 0
-    kids = 0
-    for age in args:
-        if 19 <= age:
-            total = total + 10000
-            adult = adult + 1
-        else:
-            total = total + 3000
-            kids = kids + 1
-    return [len(args), adult, kids, total]
-n = int(input())
-ages = []
-for i in range(n):
-    ages.append(random.randint(1,50))
+    print(42)
 
-results = calculate_fee(ages)
-print(f'{results[0]}명 방문 하셨고 어른 {results[1]}명, 아이 {results[2]}명, 총 요금은 {results[-1]}입니다.')
+def call_func(f):
+    """
+    매개변수로 함수를 넘겨받아 실행
+    :param f: 매개변수가 함수
+    :return:
+    """
+    f()     # 넘겨 받은 함수 실행
 
-# 딕셔너리로 표현
-def calculate_fee(args):
+def subtract(n1, n2):
+    print(n1-n2)
+
+def run_func(f, arg1, arg2):
     """
-    놀이공원 요금 계산 프로그램
-    :param args: age in list
-    :return: {'no_of_people': 전체 인원수, 'no_of_adult':어른 수, 'no_of_kid' : 아이수, 'total_fee': 총 금액}
+    함수를 매개 변수로 받아 함수 안에서 해당 함수를 실행
+    :param f: 첫 번째 인수는 함수
+    :param arg1: 정수 값
+    :param arg2: 정수 값
+    :return:
     """
-    total = 0
-    adult = 0
-    kids = 0
-    for age in ages:
-        if 19 <= age:
-            total = total + 10000
-            adult = adult + 1
-        else:
-            total = total + 3000
-            kids = kids + 1
-    return {'no_of_people': len(args), 'no_of_adult': adult, 'no_of_kid' : kids, 'total_fee': total}
-n = int(input())
-ages = []
-for i in range(n):
-    ages.append(random.randint(1,50))
-results = calculate_fee(ages)
-print(f"{results['no_of_people']}명 방문하셨고 어른 {results['no_of_adult']}명 아이 {results['no_of_kid']}명으로 총 {results['total_fee']}원 입니다.")
-# def do_nothing():
-#     pass
-#
-# mamamoo = {'화사', '솔라', '휘인', '문별'}
-# print(mamamoo.pop())        # 삭제할 값 리턴 후 삭제
-# print(mamamoo.remove('문별'))     #
-#
-#
-# print(mamamoo)
+    f(arg1, arg2)
+run_func(subtract, 99, 88)
+
+call_func(inha)
+print(type(call_func))
