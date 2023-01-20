69ebe23f7a946db3e60d2db417d18eebb4fcfc86
poow810 <poow810@naver.com>
2023-01-20 17:08:05 +0900

    day7_10-6

--- day7/day7_ex.py
+++ day7/day7_ex.py
@@ -1,27 +1,44 @@
-# 10.1
-class Thing():
-    pass
+# # 10.1
+# class Thing():
+#     pass
+#
+#
+# print(Thing)
+# example = Thing()
+# print(example)  # 출력값이 다름
+#
+# # 10.2
+# class Thing2():
+#     letters = 'abc'
+#
+# print(Thing2.letters)
+#
+# # 10.3
+# class Thing3():
+#     pass
+#
+# a = Thing3()
+# a.letters = 'abc'
+# print(a.letters)
+#
+# # 10.4
+# class Element():
+#     def __init__(self, name, symbol, number):
+#         self.name = name
+#         self.symbol = symbol
+#         self.number = number
+#
+#
+#
+#
+# element = Element('Hydrogen', 'H', 1)
+# print(element.name, element.symbol, element.number)
+#
+# # 10.5
+# el_dict = {'name':'Hydrogen', 'symbol':'H', 'number':1}
+# hydrogen = el_dict.keys()
 
-
-print(Thing)
-example = Thing()
-print(example)  # 출력값이 다름
-
-# 10.2
-class Thing2():
-    letters = 'abc'
-
-print(Thing2.letters)
-
-# 10.3
-class Thing3():
-    pass
-
-a = Thing3()
-a.letters = 'abc'
-print(a.letters)
-
-# 10.4
+# 10.6
 class Element():
     def __init__(self, name, symbol, number):
         self.name = name
@@ -29,9 +46,9 @@ class Element():
         self.number = number
 
 
-element = Element('Hydrogen', 'H', 1)
-print(element.name, element.symbol, element.number)
+    def dump(self):
+        print(self.name, self.symbol, self.number)
 
-# 10.5
-el_dict = {'name':'Hydrogen', 'symbol':'H', 'number':1}
-hydrogen = el_dict.keys()
+
+hydrogen = Element('Hydrogen', 'H', 1)
+hydrogen.dump()
