f99057101bee5b0331b7145083b59b6da3ceaa8b
poow810 <poow810@naver.com>
2023-02-03 11:45:10 +0900

    day15 linear list 05

--- day09/day09.py
+++ day09/day09.py
@@ -13,28 +13,21 @@ def insert_data(index, pokemon):
 
 
 def delete_data(index):
+    if index < 0 or index > len(pokemons):
+        print("데이터를 삭제할 범위를 벗어났습니다.")
+        return
 
+    pokemons[index] = None  # 데이터 삭제
 
-        if index < 0 or index > len(pokemons):
-            print("데이터를 삭제할 범위를 벗어났습니다.")
-            return
+    for i in range(index + 1, len(pokemons)):
+        pokemons[i] = None
 
-        len_pokemons = len(pokemons)
-        pokemons[index] = None  # 데이터 삭제
-
-        for i in range(index + 1, len_pokemons):
-            pokemons[i - 1] = pokemons[i]
-            pokemons[i] = None
-
-        del (pokemons[len_pokemons - 1])
-
+    for i in range(index + 1, 6):
+        pokemons.pop()
 
 
 if __name__ == "__main__":
     pokemons = ["피카츄", "라이츄", "꼬부기", "파이리", "이상해씨"]
     print(pokemons)
-#    insert_data(2, '어니부기')
     delete_data(1)  # 라이츄 지움
-    print(pokemons)
-    delete_data(6)
     print(pokemons)
