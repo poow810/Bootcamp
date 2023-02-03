3f234d1e4819d94dd51c10b4cd49c25ad8348881
poow810 <poow810@naver.com>
2023-02-03 11:16:30 +0900

    day15 linear list 02

--- day09/day09.py
+++ day09/day09.py
@@ -1,15 +1,20 @@
-pokemons = []  # 빈 배열
+def insert_data(index, pokemon):
+    if index < 0 or index > len(pokemons):
+        print("Out of range!")
+        return
 
+    pokemons.append(None)  # 빈칸 추가
 
-def add_data(pokemon):
-    pokemons.append(None)
-    pokemons[- 1] = pokemon
+    for i in range(len(pokemons) - 1, index, -1):
+        pokemons[i] = pokemons[i - 1]
+        pokemons[i - 1] = None
 
+    pokemons[index] = pokemon  # 지정한 위치에 친구 추가
 
-add_data('피카츄')
-add_data('라이츄')
-add_data('파이리')
-add_data('꼬부기')
-add_data('이상해씨')
-
-print(pokemons)
+if __name__ == "__main__":
+    pokemons = ["피카츄", "라이츄", "꼬부기", "파이리", "이상해씨"]
+    print(pokemons)
+    insert_data(2, '어니부기')
+    print(pokemons)
+    insert_data(6, '거북왕')
+    print(pokemons)
