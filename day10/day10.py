a60c7ac671a9156fe003234b9c58c28f978cf194
poow810 <poow810@naver.com>
2023-02-06 18:12:18 +0900

    0206 homework01

--- day10/day10.py
+++ day10/day10.py
@@ -1,30 +1,68 @@
-stack = [None for _ in range(5)]
+def is_stack_full():
+    global Size, stack, top
+    if top >= Size - 1:
+        return True
+    return False
+
+
+def is_stack_empty():
+    global Size, stack, top
+    if top >= -1:
+        return True
+    else:
+        return False
+
+
+def push(data):
+    global Size, stack, top
+    if is_stack_full():
+        print("Stack is full!")
+        return
+    top = top + 1
+    stack[top] = data
+
+
+def pop():
+    global Size, stack, top
+    if is_stack_empty():
+        print("Stack is empty!")
+        return None
+    temp = stack[top]
+    stack[top] = None
+    top = top - 1
+    return temp
+
+
+def peek():
+    global Size, stack, top
+    if is_stack_empty():
+        print("Stack is empty!")
+        return None
+    return stack[top]
+
+
+Size = int(input("Stack Size : "))
+stack = [None for _ in range(Size)]
 top = -1
 
-top = top + 1
-stack[top] = "커피"
-top = top + 1
-stack[top] = "녹차"
-top = top + 1
-stack[top] = "꿀물"
+if __name__ == "__main__":
+    while True:
+        menu = input("삽입(I)/추출(E)/확인(V)/종료(X) 중 하나를 선택 ==> ")
+        if menu == 'X' or menu == 'x':
+            break
+        elif menu == 'I' or menu == 'i':
+            data = input("입력할 데이터 ==> ")
+            push(data)
+            print("스택 상태 : ", stack)
+        elif menu == 'E' or menu == 'e':
+            data = pop()
+            print("추출된 데이터 ==> ", data)
+            print("스택 상태 : ", stack)
+        elif menu == 'V' or menu == 'v':
+            data = peek()
+            print("확인된 데이터 ==> ", data)
+            print("스택 상태 : ", stack)
+        else:
+            print("입력이 잘못됨")
 
-for i in range(len(stack)-1, -1, -1):
-    print(stack[i])
-
-data = stack[top]
-stack[top] = None
-top = top - 1
-print(data)
-
-data = stack[top]
-stack[top] = None
-top = top - 1
-print(data)
-
-data = stack[top]
-stack[top] = None
-top = top - 1
-print(data)
-
-for i in range(len(stack)-1, -1, -1):
-    print(stack[i])
+print("프로그램 종료!")
