069363f288d54ea477633b25cc548b9435b35be9
poow810 <poow810@naver.com>
2023-02-07 21:02:20 +0900

    day11_practice_03

--- day11/day11_practice.py
+++ day11/day11_practice.py
@@ -1,61 +1,50 @@
-# 응용 예제 02 - 콜센터의 응답 대기 시간 계산하기
-def is_queue_empty():
-    global waiting, Size, front, rear
-    if front == rear:
-        return True
-    else:
-        return False
+# 응용 예제 1 - 편의점에서 판매된 물건 목록 출력하기
+import random
 
 
-def is_quene_full():
-    global waiting, Size, front, rear
-    if front == (rear+1) % Size:
-        return True
-    else:
-        return False
+class TreeNode():
+    def __init__(self):
+        self.left = None
+        self.data = None
+        self.right = None
 
 
-def en_queue(data):
-    global calling, Size, front, rear
-
-    if is_quene_full():
+def inorder(node):
+    if node == None:
         return
-    rear = (rear+1) % Size
-    calling[rear] = data
+    inorder(node.left)
+    print(node.data, end=' ')
+    inorder(node.right)
 
 
-def de_queue():
-    global calling, Size, front, rear
+menu = ['레쓰비캔커피', '도시락', '삼각김밥', '코카콜라', '삼다수', '츄파춥스']
+sell = [random.choice(menu) for i in range(20)]
+print(f'오늘 판매된 물건(중복O) --> {sell}')
 
-    if is_queue_empty():
-        return
-    front = (front+1) % Size
-    data = calling[front]
-    calling[front] = None
-    return data
+node = TreeNode()
+node.data = sell[0]
+root = node
 
 
-def wait_time():
-    global calling, Size, front, rear
-    time = 0
-    for i in range((front+1)%Size, (rear+1)%Size):
-        time = time + calling[i][1]
-    return time
+for food in sell[1:]:
+    node = TreeNode()
+    node.data = food
 
+    current = root
+    while True:
+        if food == current.data:
+            break
+        if food < current.data:
+            if current.left == None:
+                current.left = node
+                break
+            current = current.left
+        else:
+            if current.right == None:
+                current.right = node
+                break
+            current = current.right
 
+print('오늘 판매된 종류(중복X)--> ', end='')
+print(inorder(root))
 
-Size = 6
-calling = [None for _ in range(Size)]
-front = rear = 0
-
-if __name__ == "__main__":
-    waiting = [('사용', 9), ('고장', 3), ('환불', 4), ('환불', 4), ('고장', 3)]
-
-    for call in waiting:
-        print(f"귀하의 대기 예상시간은 {wait_time()} 분입니다.")
-        print(f"현재 대기 콜 --> {calling}")
-        en_queue(call)
-        print()
-
-    print(f"최종 대기 콜 --> {calling}")
-    print("프로그램 종료!")
