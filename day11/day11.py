278b3a4b872e2ea4f09bd7cfd2a299f5cdbe1168
poow810 <poow810@naver.com>
2023-02-07 13:05:15 +0900

    queue 0207

--- day11/day11.py
+++ day11/day11.py
@@ -1,56 +1,76 @@
-# 간단 구현
-# 데이터 삽입
-queue = [None, None, None, None, None]
-front = rear = -1
-rear += 1
-queue[rear] = "화사"
-rear += 1
-queue[rear] = "문별"
-rear += 1
-queue[rear] = "솔라"  # rear은 2인 상태
-
-# 데이터 추출
-front += 1
-data = queue[front]
-queue[front] = None   # queue의 0번째를 데이터로 추출 후 None
-
-# 일반 구현
-# 큐가 꽉 찼는지 확인하는 함수
-def is_quene_full():
-    global Size, queue, front, rear
-    if (rear == Size-1):
+## 함수 선언 부분 ##
+def is_queue_full():
+    global SIZE, queue, front, rear
+    if ((rear + 1) % SIZE == front):
         return True
     else:
         return False
 
-# 큐가 비었는지 확인하는 함수
+
 def is_queue_empty():
-    global Size, queue, front, rear
+    global SIZE, queue, front, rear
     if (front == rear):
         return True
     else:
         return False
-# 큐에 데이터를 삽입하는 함수
-def en_queue():
-    global Size, queue, front, rear
-    if (is_quene_full()):
+
+
+def en_queue(data):
+    global SIZE, queue, front, rear
+    if (is_queue_full()):
+        print("큐가 꽉 찼습니다.")
         return
-    rear += 1
+    rear = (rear + 1) % SIZE
     queue[rear] = data
 
-# 큐의 데이터를 추출하는 함수
+
 def de_queue():
-    global Size, queue, front, rear
+    global SIZE, queue, front, rear
     if (is_queue_empty()):
-        return
-    front += 1
+        print("큐가 비었습니다.")
+        return None
+    front = (front + 1) % SIZE
     data = queue[front]
     queue[front] = None
     return data
 
-# 큐에서 front+1 위치의 데이터를 확인하는 함수
+
 def peek():
-    global Size, queue, front, rear
+    global SIZE, queue, front, rear
     if (is_queue_empty()):
-        return
-    return queue[(front+1)]
+        print("큐가 비었습니다.")
+        return None
+    return queue[(front + 1) % SIZE]
+
+
+## 전역 변수 선언 부분 ##
+SIZE = int(input("큐의 크기를 입력하세요 ==> "))
+queue = [None for _ in range(SIZE)]
+front = rear = 0
+
+## 메인 코드 부분 ##
+if __name__ == "__main__":
+    select = input("삽입(I)/추출(E)/확인(V)/종료(X) 중 하나를 선택 ==> ")
+
+    while (select != 'X' and select != 'x'):
+        if select == 'I' or select == 'i':
+            data = input("입력할 데이터 ==> ")
+            en_queue(data)
+            print("큐 상태 : ", queue)
+            print("front : ", front, ", rear : ", rear)
+        elif select == 'E' or select == 'e':
+            data = de_queue()
+            print("추출된 데이터 ==> ", data)
+            print("큐 상태 : ", queue)
+            print("front : ", front, ", rear : ", rear)
+        elif   select == 'V' or select == 'v':
+            data = peek()
+            print("확인된 데이터 ==> ", data)
+            print("큐 상태 : ", queue)
+            print("front : ", front, ", rear : ", rear)
+        else:
+            print("입력이 잘못됨")
+
+        select = input("삽입(I)/추출(E)/확인(V)/종료(X) 중 하나를 선택 ==> ")
+
+    print("프로그램 종료!")
