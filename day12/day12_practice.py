12adab9d8f6fd99d48d0c1bce28a6ebad18ca37a
poow810 <poow810@naver.com>
2023-02-08 23:47:59 +0900

    ììday12 practice_02

--- day12/day12_practice.py
+++ day12/day12_practice.py
@@ -1,68 +1,96 @@
-# 응용 예제 1 - 허니버터칩이 가장 많이 남은 편의점 찾기
+# 응용 예제 2 - 효율적인 케이블망 구성하기
+from operator import itemgetter
+
+
 class Graph:
     def __init__(self, size):
         self.size = size
-        self.graph = [[0 for _ in range(size)] for _ in range(size)]
+        self.graph = [[0 for _ in range(size)] for _ in range (size)]
 
 
-# graph 출력함수
 def print_graph(g):
     print(' ', end=' ')
-    for i in range(gsize):
-        print("%7s"%store_array[i][0], end='')
+    for i in range(g.size):
+        print(city[i], end = ' ')
     print()
-    for j in range(gsize):
-        print("%9s"%store_array[j][0], end=' ')
-        for k in range(gsize):
-            print("%3d"%g.graph[j][k], end=' ')
+    for j in range(g.size):
+        print(city[j], end = ' ')
+        for k in range(g.size):
+            print("%2d"%g.graph[j][k], end = ' ')
         print()
 
 
+def find_vertex(g, findvtx):
+    stack = []
+    visit_array = []
+    current = 0
+    stack.append(current)
+    visit_array.append(current)
 
-
-# 전역 변수 설정
-G1 = None
-store_array = [["Gs25", 30], ["CU", 60], ["Seven", 10], ["Mini", 90], ["Emart", 40]]
-Gs25, CU, Seven, Mini, Emart = 0, 1, 2, 3, 4
-stack = []
-visit_array = []
-
-gsize = 5
-G1 = Graph(gsize)
-G1.graph[0][1] = 1; G1.graph[0][2] = 1
-G1.graph[1][0] = 1; G1.graph[1][2] = 1; G1.graph[1][3] = 1
-G1.graph[2][0] = 1; G1.graph[2][1] = 1; G1.graph[2][3] = 1
-G1.graph[3][1] = 1; G1.graph[3][2] = 1; G1.graph[3][4] = 1
-G1.graph[4][3] = 1
-# 방문
-current = 0
-max_snack = current     # 편의점 번호를 0으로 지정해놓고 서로 다른 편의점 방문할 때마다 크기 비교
-max_count = store_array[current][1]
-stack.append(current)
-visit_array.append(current)
-
-while len(stack) != None:
-    next = None
-    for i in range(gsize):
-        if G1.graph[current][i] == 1:
-            if i in visit_array:
-                pass
-            else:
-                next = i
-                break
-
-    if next != None:
-        current = next
-        stack.append(current)
-        visit_array.append(current)
-        if store_array[current][1] > max_count:
-            max_count = store_array[current][1]
-            max_snack = current
+    while len(stack) != 0:
+        next = None
+        for i in range(g.size):
+            if g.graph[current][i] != 0:
+                if i in visit_array:
+                    pass
+                else:
+                    next = i
+                    break
+        if next != None:
+            current = next
+            stack.append(current)
+            visit_array.append(current)
+        else:
+            current = stack.pop()
+    if findvtx in visit_array:
+        return True
     else:
-        current = stack.pop()
-        break        # stack에서 아예 추출할 data가 없을 때까지 pop()을 해서 오류 발생 break 사용
+        return False
 
 
-print("## 편의점 그래프 ##")
+G1 = None
+city = ['서울', '뉴욕', '북경', '방콕', '런던', '파리']
+서울, 뉴욕, 북경, 방콕, 런던, 파리 = 0, 1, 2, 3, 4, 5
+
+gsize = 6
+G1 = Graph(gsize)
+G1.graph[0][1] = 80; G1.graph[0][2] = 10
+G1.graph[1][0] = 80; G1.graph[1][3] = 70; G1.graph[1][2] = 40
+G1.graph[2][0] = 10; G1.graph[2][1] = 40; G1.graph[2][3] = 50
+G1.graph[3][1] = 70; G1.graph[3][2] = 50; G1.graph[3][4] = 30; G1.graph[3][5] = 20
+G1.graph[4][3] = 30; G1.graph[4][5] = 60
+G1.graph[5][3] = 20; G1.graph[5][4] = 60
 print_graph(G1)
-print(f"허니 버터칩 최대 보유 편의점 --> {store_array[max_snack][0],store_array[max_snack][1]}")
+
+edge_array = []
+for i in range(gsize):
+    for j in range(gsize):
+        if G1.graph[i][j] != 0:
+            edge_array.append([G1.graph[i][j], i, j])
+
+edge_array = sorted(edge_array, key = itemgetter(0), reverse = False)
+new = []
+for i in range(0, len(edge_array), 2):
+    new.append(edge_array[i])
+
+index = 0
+while len(new) > 5:
+    start = new[index][1]
+    end = new[index][2]
+    save = new[index][0]
+
+    G1.graph[start][end] = 0
+    G1.graph[end][start] = 0
+
+    point_start = find_vertex(G1, start)
+    point_end = find_vertex(G1, end)
+
+    if point_start and point_end:
+        del(new[index])
+    else:
+        G1.graph[start][end] = save
+        G1.graph[end][start] = save
+        index += 1
+
+
+print_graph(G1)
