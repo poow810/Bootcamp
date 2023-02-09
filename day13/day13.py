63e3f80cb5374373184fa515ac4b1abfd36dd77b
poow810 <poow810@naver.com>
2023-02-09 23:35:33 +0900

    정렬 시간비교 해보기

--- day13/day13.py
+++ day13/day13.py
@@ -1,25 +1,16 @@
-# 퀵 정렬의 일반적인 구현
-def quick_sort(ary, start, end):
-    if end <= start:
-        return
-    low = start
-    high = end
-
-    pivot = ary[(low+high)//2]
-    while low <= high:
-        while ary[low] < pivot:
-            low += 1
-        while ary[high] > pivot:
-            high -= 1
-        if low <= high:
-            ary[low], ary[high] = ary[high], ary[low]
-            low, high = low +1, high -1
-
-    mid = low
-    quick_sort(ary, start, mid-1)
-    quick_sort(ary, mid, end)
+def mid_num(ary):
+    n = len(ary)
+    for i in range(0, n-1):
+        idx = i
+        for j in range(i+1, n):
+            if ary[idx] > ary[j]:
+                idx = j
+        temp = ary[i]
+        ary[i] = ary[idx]
+        ary[idx] = temp
+        ary = ary[1:-2]
     return ary
 
 
-array = [188, 150, 168, 162, 105, 120, 177, 50]
-print(quick_sort(array, 0, len(array)-1))
+array = [1, 36, 30, 70, 90, 1000, 4000]
+print(mid_num(array))
--- day13/day13_practice.py
+++ day13/day13_practice.py
@@ -1,5 +1,9 @@
-# 응용 예제 2 - 2차원 배열의 중앙값 찾기
-def sort_array(ary):
+# 응용 예제 1 - 선택 정렬과 퀵 정렬의 성능 비교하기
+import random
+import time
+
+
+def selection(ary):
     n = len(ary)
     for i in range(0, n-1):
         idx = i
@@ -12,16 +16,42 @@ def sort_array(ary):
     return ary
 
 
-array = [[53, 33, 250, 44],
-         [88, 1, 67, 23],
-         [199, 222, 38, 47],
-         [155, 145, 20, 99]]
-new_array = []
-for i in range(len(array)):
-    for j in range(len(array[i])):
-        new_array.append(array[i][j])
-print('1차원 변경 후, 정렬 전 -->', end= ' ')
-print(new_array)
-print('1차원 변경 후, 정렬 후 -->', end= ' ')
-print(sort_array(new_array))
-print(f"중앙값 --> {new_array[len(new_array)//2]}")
+def quick_sort(ary, start, end):
+    if start >= end:
+        return
+    low = start
+    high = end
+
+    pivot = ary[(low+high)//2]
+    while low <= high:
+        while ary[low] < pivot:
+            low += 1
+        while ary[high] > pivot:
+            high -= 1
+        if low <= high:
+            ary[low], ary[high] = ary[high], ary[low]
+            low, high = low+1, high - 1
+
+    mid = low
+    quick_sort(ary, start, mid-1)
+    quick_sort(ary, mid, end)
+
+
+count_array = [1000, 10000, 12000, 15000]
+for i in count_array:
+    temp = [random.randint(1,15000) for _ in range(i)]
+    select = temp[:]
+    quick = temp[:]
+
+    print(f"## 데이터 수 --> {i}")
+    start = time.time()
+    selection(count_array)
+    end = time.time()
+    print(f"선택 정렬 --> {end-start:.3f}")
+    start = time.time()
+    quick_sort(count_array, 0, len(count_array)-1)
+    end = time.time()
+    print(f"퀵 정렬 --> {end-start:.3f}")
+
+    i *= 5
+
