2f0e332f94dc3c6d53c0ce29241780afafed222c
poow810 <poow810@naver.com>
2023-02-09 20:41:32 +0900

    삽입 ì삽입 정렬의 효율적 구현

--- day13/day13.py
+++ day13/day13.py
@@ -1,14 +1,13 @@
-# 배열에서 자신이 삽입될 위치를 찾는 함수
-def find_index(ary, data):
-    findidx = -1
-    for i in range(0, len(ary)):
-        if ary[i] > data:
-            findidx = i
-            break
-    if findidx == -1:       # 아무것도 못찾았을 경우에 맨 마지막 위치
-        return [findidx]
-    else:
-        return [findidx]
+# 삽입 정렬 효율적 구현
+def insertion(ary):
+    n = len(ary)
+    for i in range(0,n):
+        for j in range(i, 0, -1):
+            if ary[j-1] < ary[j]:
+                ary[j-1], ary[j] = ary[j], ary[j-1]
+    return ary
 
-test = [33, 40, 70]
-print(find_index(test, 50))
+array = [15, 14, 17, 20, 66, 99]
+
+new_array = insertion(array)
+print(new_array)
