15b1748b3d6932d3b72aad2a3f17599a6280e217
poow810 <poow810@naver.com>
2023-02-09 22:08:56 +0900

    중복 값을 고려한 퀵 정렬

--- day13/day13.py
+++ day13/day13.py
@@ -1,20 +1,24 @@
-# 퀵 정렬의 간단한 구현
+# 중복 값을 고려한 퀵 정렬
 def quick_sort(ary):
     n = len(ary)
     if n <= 1:
         return ary
 
-    pivot = ary[n // 2]     # 중간값을 기준으로 지정
-    left_array, right_array = [], []
+    pivot = ary[n//2]
+    left_array, right_array, mid_array = [], [], []
 
     for num in ary:
-        if num < pivot:
-            left_array.append(num)
-        elif num > pivot:
+        if num > pivot:
             right_array.append(num)
-    return quick_sort(left_array) + [pivot] + quick_sort(right_array)
+        elif num < pivot:
+            left_array.append(num)
+        else:
+            mid_array.append(num)
+
+    return quick_sort(left_array)+mid_array+quick_sort(right_array)
+# mid_array는 함수로 반환하지 않는 이유?
+# num = pivot 값으로 기준 값이기 때문에 기준이 변하면 안됨
 
 
-array = [188, 150, 168, 162, 105, 120, 177, 50]
-
+array = [161, 2, 66, 90, 105, 144, 66, 161, 90]
 print(quick_sort(array))
