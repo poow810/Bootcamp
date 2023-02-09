e7abd5cb6ff6d97770e7fafa651566f13ba3f2d5
poow810 <poow810@naver.com>
2023-02-09 16:51:08 +0900

    재귀함수를 통한 피보나치 수

--- day13/day13.py
+++ day13/day13.py
@@ -1,25 +1,12 @@
-import random
-# 재귀 함수를 통한 숫자 합계 내기
-def add_number(num):
-    if num <= 1:
-        return 1
-    return num+add_number(num-1)
-
-# 팩토리얼을 재귀 함수로 구현
-def factorial(num):
-    if num <= 1:
-        return 1
-    return num*factorial(num-1)
-
-
-# 배열의 합 계산하기
-def array_sum(arr, n):
+# 피보나치 수
+def fibo(n):
     if n <= 0:
-        return arr[0]
-    return arr[n]+array_sum(arr, n-1)
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibo(n - 1) + fibo(n - 2)
 
 
-array = [random.randint(0, 255) for _ in range(10)]
-print(array)
-print(array_sum(array, len(array)-1))
-
+for i in range(2, 20):
+    print(fibo(i), end=' ')
