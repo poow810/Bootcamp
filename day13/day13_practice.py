3cfc00dd53272e4cecfc4ca8be2277720021b8b7
poow810 <poow810@naver.com>
2023-02-09 21:44:16 +0900

    ch11 practice02

--- day13/day13_practice.py
+++ day13/day13_practice.py
@@ -1,19 +1,27 @@
-# 응용 예제 1 - 성적별로 조 편성하기
-def find_index(ary):
+# 응용 예제 2 - 2차원 배열의 중앙값 찾기
+def sort_array(ary):
     n = len(ary)
-    for i in range(1, n):
-        for j in range(i, 0, -1):
-            if ary[j-1][1] > ary[j][1]:
-                ary[j-1], ary[j] = ary[j], ary[j-1]
+    for i in range(0, n-1):
+        idx = i
+        for j in range(i+1, n):
+            if ary[idx] > ary[j]:
+                idx = j
+        temp = ary[i]
+        ary[i] = ary[idx]
+        ary[idx] = temp
     return ary
 
 
-array = [['선미', 88], ['초아', 99], ['화사', 71],
-         ['영탁', 78], ['영웅', 67], ['민호', 92]]
-print("정렬 전 --> ", end=' ')
-print(array)
-print("정렬 후 --> ", end=' ')
-print(find_index(array))
-print("## 성적별 조 편성표 ##")
-for i in range(0, len(array)//2):
-    print(f"{array[i][0]} : {array[len(array)-i-1][0]}")
+array = [[53, 33, 250, 44],
+         [88, 1, 67, 23],
+         [199, 222, 38, 47],
+         [155, 145, 20, 99]]
+new_array = []
+for i in range(len(array)):
+    for j in range(len(array[i])):
+        new_array.append(array[i][j])
+print('1차원 변경 후, 정렬 전 -->', end= ' ')
+print(new_array)
+print('1차원 변경 후, 정렬 후 -->', end= ' ')
+print(sort_array(new_array))
+print(f"중앙값 --> {new_array[len(new_array)//2]}")
