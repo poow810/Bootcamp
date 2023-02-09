fbc531e2bb16fac9e457ea18de9298ae7162d94a
poow810 <poow810@naver.com>
2023-02-09 20:24:27 +0900

    삽입 위치 찾는 함수

--- day13/day13.py
+++ day13/day13.py
@@ -1,17 +1,14 @@
-# 개선된 선택 정렬
-def selection_sort(ary):
-    n = len(ary)
-    for i in range(0, n - 1):
-        findex = i
-        for j in range(i + 1, n):
-            if ary[findex] > ary[j]:
-                findex = j
-        temp = ary[i]
-        ary[i] = ary[findex]
-        ary[findex] = temp
-    return ary
+# 배열에서 자신이 삽입될 위치를 찾는 함수
+def find_index(ary, data):
+    findidx = -1
+    for i in range(0, len(ary)):
+        if ary[i] > data:
+            findidx = i
+            break
+    if findidx == -1:       # 아무것도 못찾았을 경우에 맨 마지막 위치
+        return [findidx]
+    else:
+        return [findidx]
 
-
-array = [1, 12, 33, 4, 51, 6, 100, 25]
-after_array = selection_sort(array)
-print(after_array)
+test = [33, 40, 70]
+print(find_index(test, 50))
