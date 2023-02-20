1fa25c138496c0fc0d8e5ed1cd59f9630d010c88
poow810 <poow810@naver.com>
2023-02-20 20:55:51 +0900

    13-3 이진탐색

--- day26/Day26.py
+++ day26/Day26.py
@@ -1,16 +1,23 @@
 def find_index(ary, data):
     pos = -1
-    for i in range(len(ary)):
-        if ary[i] == data:
-            pos = i
-            break
-        elif ary[i] > data:
-            break
-    return pos
+    start = 0
+    end = len(ary)-1
+    while end >= start:
+        mid = (start+end)//2
+        if data == ary[mid]:
+            return mid
+        elif data > ary[mid]:
+            start = mid + 1
+        else:
+            end = mid + 1
+
 
 
 data_array = [188, 150, 168, 162, 105, 120, 177, 50]
 find_data = int(input())
 data_array = sorted(data_array)
-
-print(find_index(data_array, find_data))
+position = find_index(data_array, find_data)
+if position == -1:
+    print("찾는 data가 data_array에 없음")
+else:
+    print(f"찾는 data가 {position}에 있음")
