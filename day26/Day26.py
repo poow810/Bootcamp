752ed3c2ce632d80bb616c3864fa6c055ab82eb5
poow810 <poow810@naver.com>
2023-02-20 20:29:21 +0900

    self study 13-1

--- day26/Day26.py
+++ day26/Day26.py
@@ -1,16 +1,10 @@
 def find_index(ary, data):
     pos = -1
-    for i in range(len(ary)):
-        if ary[i] == data:
-            pos = i
-            break
-    return pos
+    position = [x for x in range(len(ary)) if ary[x] == data]
+    return position
 
 
-data_array = [188, 150, 168, 162, 105, 120, 177, 50]
-find_data = int(input())
-position = find_index(data_array, find_data)
-if position == -1:
-    print("찾는 data가 array에 없습니다.")
-else:
-    print(f"찾는 data가 {position}에 있습니다.")
+data_array = [188, 50, 150, 168, 50, 162, 105, 120, 177, 50]
+find_data = 50
+
+print(find_index(data_array, find_data))
