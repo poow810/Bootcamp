b925a03a729edddccd5e174e4215a38e46530fc8
poow810 <poow810@naver.com>
2023-02-20 20:40:22 +0900

    sorted list의 순차검색

--- day26/Day26.py
+++ day26/Day26.py
@@ -1,10 +1,16 @@
 def find_index(ary, data):
     pos = -1
-    position = [x for x in range(len(ary)) if ary[x] == data]
-    return position
+    for i in range(len(ary)):
+        if ary[i] == data:
+            pos = i
+            break
+        elif ary[i] > data:
+            break
+    return pos
 
 
-data_array = [188, 50, 150, 168, 50, 162, 105, 120, 177, 50]
-find_data = 50
+data_array = [188, 150, 168, 162, 105, 120, 177, 50]
+find_data = int(input())
+data_array = sorted(data_array)
 
 print(find_index(data_array, find_data))
